b609a64083fba0849a26924ffea131a1f875060a
Piyush Negi <piyush.negi@mindstix.com>
2022-01-02 16:02:07 +0530

    Fixed Switch imort to Routes in MainRouter.

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./core/Home";
 
 const MainRouter = () => {
   return (
     <div>
-      <Switch>
-        {/* <Route exact path="/" component={Home}/> */}
-        <h1>Hl</h1>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
